97fc8cbbff52d646c8ba59701984c96d36d6e1e7
Endri Deliu <endri.deliu@salesforce.com>
2020-12-19 13:48:55 -0800

    add tests for GenomeExplainer and GenomeEstimator

--- service-modelstore/sdk/src/python/test/test_sdk.py
+++ service-modelstore/sdk/src/python/test/test_sdk.py
@@ -6,18 +6,160 @@ import io
 import json
 import pickle
 
+import numpy as np
+
 mock_tfload = MagicMock()
+mock_keras = MagicMock()
+mock_models = MagicMock()
+mock_layers = MagicMock()
+mock_preprocessors = MagicMock()
+
 mock_tfload.saved_model = MagicMock()
 mock_tfload.saved_model.load.return_value = {"id": "blob-123"}
 sys.modules["tensorflow"] = mock_tfload
+sys.modules["tensorflow.keras"] = mock_keras
+sys.modules["tensorflow.keras.models"] = mock_models
+sys.modules["tensorflow.keras.layers"] = mock_layers
+sys.modules["tensorflow.keras.preprocessing.image"] = mock_preprocessors
+
+sys.modules["six"] = MagicMock()
+sys.modules["attr"] = MagicMock()
+
+
+mock_numpy = MagicMock()
+mock_numpy.saved_model = MagicMock()
+mock_numpy.saved_model.load.return_value = {"id": "blob-123"}
+sys.modules["numpy"] = mock_numpy
+
+
+mock_shap = MagicMock()
+mock_shap.TreeExplainer = MagicMock()
+mock_shap.LinearExplainer = MagicMock()
+mock_shap.TreeExplainer.shap_values.return_value = [2,2,2]
+mock_shap.LinearExplainer.shap_values.return_value = [3,3,3]
+sys.modules["shap"] = mock_shap
+
 
 from ..modelstore import client
+from ..modelstore import explainer
+from ..modelstore import estimator
 
 from ..modelstore.meta_extractor import SKMetaExtractor
 from ..modelstore.meta_extractor import XGMetaExtractor
 from ..modelstore.meta_extractor import TFMetaExtractor
 from ..modelstore.meta_extractor import LightGBMMetaExtractor
 from ..modelstore.meta_extractor import CatBoostMetaExtractor
+
+
+class TestGenomeExplainer(TestCase):
+
+    def test_tree_explainer(self):
+
+        class sklearnTreeMock(MagicMock):
+            pass
+
+        estimatorMock = sklearnTreeMock()
+        genomeExplainer = explainer.GenomeExplainer(estimatorMock, "tabular")
+
+        self.assertEqual(genomeExplainer.model_type, "tree")
+
+        genomeExplainer.explainer.shap_values.return_value = [2,2,2]
+        res = genomeExplainer.explain([1,2,3])
+        self.assertEqual(res, [2,2,2])
+
+
+    def test_linear_explainer(self):
+
+        class sklearnLinearMock(MagicMock):
+            pass
+
+        estimatorMock = sklearnLinearMock()
+        genomeExplainer = explainer.GenomeExplainer(estimatorMock, "tabular")
+
+        self.assertEqual(genomeExplainer.model_type, "linear")
+
+        genomeExplainer.explainer.shap_values.return_value = [3,3,3]
+        res = genomeExplainer.explain([1,2,3])
+        self.assertEqual(res, [3,3,3])
+
+
+
+    @patch(explainer.__name__ + ".explain_prediction")
+    def test_image_explainer(self, mock_pred):
+
+        class tensorflowImageMock(MagicMock):
+            pass
+
+        estimatorMock = tensorflowImageMock()
+        genomeExplainer = explainer.GenomeExplainer(estimatorMock, "image")
+
+        self.assertEqual(genomeExplainer.model_type, "nn")
+
+        #mock_image_explain.explain_prediction.return_value = [2,2,2]
+        mock_pred.return_value = [2,2,2]
+        res = genomeExplainer.explain(np.array([[1,2,3],[1,2,3],[1,2,3]]))
+        self.assertEqual(res, [2,2,2])
+
+
+
+
+    @patch(explainer.__name__ + ".TextExplainer", create=True)
+    @patch(explainer.__name__ + ".format_as_dict", create=True, side_effect=lambda x: x)
+    def test_text_explainer(self, mock_format, mock_explainer):
+
+        class tensorflowTextMock(MagicMock):
+            pass
+
+
+        mock_instance = mock_explainer.return_value
+        mock_instance.fit = MagicMock()
+        mock_instance.explain_prediction = MagicMock(return_value=[2,2,2])
+
+        estimatorMock = tensorflowTextMock()
+        estimatorMock.func_pred = MagicMock()
+        estimatorMock.func_pred.return_value = 1
+        genomeExplainer = explainer.GenomeExplainer(estimatorMock, "text", estimator_predict="func_pred")
+
+        self.assertEqual(genomeExplainer.model_type, "nn")
+
+
+        res = genomeExplainer.explain(np.array([[1,2,3],[1,2,3],[1,2,3]]), estimator=estimatorMock)
+        self.assertEqual(res, [2,2,2])
+        mock_instance.explain_prediction.assert_called()
+
+
+
+
+    @patch(estimator.__name__ + ".GenomeExplainer")
+    def test_genome_estimator(self, mock_explainer):
+
+        class tensorflowTextMock(MagicMock):
+            pass
+
+        estimatorMock = tensorflowTextMock()
+        estimatorMock.func_pred = MagicMock()
+        estimatorMock.func_pred.return_value = 1
+
+        mock_preprocessor = MagicMock()
+        mock_instance = mock_explainer.return_value
+        mock_instance.explain = MagicMock(return_value=[2,2,2])
+
+        genomeEstimator = estimator.GenomeEstimator(estimatorMock,
+           data_preprocessor = mock_preprocessor,
+           modality="text",
+           estimator_predict="func_pred")
+
+        self.assertTrue(genomeEstimator.explainer != None)
+
+
+        res = genomeEstimator.explain(np.array([[1,2,3],[1,2,3],[1,2,3]]))
+
+        mock_preprocessor.assert_called()
+        mock_instance.explain.assert_called()
+        self.assertEqual(res, [2,2,2])
+
+
+
 
 
 
@@ -58,7 +200,7 @@ class TestModelStoreSave(TestCase):
 
         cm = MagicMock()
         cm.getcode.return_value = 200
-        cm.read.return_value = '[{"id": "model-123", "framework":"sklearn",  "artifactBlob":{"ref":"blob-123"}}]'
+        cm.read.return_value = '[{"id": "model-123", "application": "search", "canonicalName": "/search/pipeline", "framework":"sklearn", "artifactBlob":{"ref":"blob-123"}}]'
         cm.__enter__.return_value = cm
 
         cm1 = MagicMock()
@@ -98,7 +240,7 @@ class TestModelStoreSave(TestCase):
 
         cm = MagicMock()
         cm.getcode.return_value = 200
-        cm.read.return_value = '[{"id": "model-123", "framework":"tensorflow",  "artifactBlob":{"ref":"blob-123"}}]'
+        cm.read.return_value = '[{"id": "model-123", "application": "search", "canonicalName": "/search/pipeline", "framework":"tensorflow", "artifactBlob":{"ref":"blob-123"}}]'
         cm.__enter__.return_value = cm
 
         cm1 = MagicMock()
